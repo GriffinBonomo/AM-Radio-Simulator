eee1c154958c283d664c2d9dc51e596fa224dbec
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2025-10-09 14:55:09 -0400

    Add crackle effect

--- core/effects.py
+++ core/effects.py
@@ -13,3 +13,21 @@ def add_noise_bandlimited(signal: np.ndarray, sample_rate: int, noise_level: flo
     filtered = butter_filter(noise, sample_rate, low_cutoff, "high", order)
     filtered = butter_filter(filtered, sample_rate, high_cutoff, "low", order)
     return signal + filtered
+
+def add_crackle(signal: np.ndarray, sample_rate: int, density=0.001, intensity=0.3):
+    crackle_track = np.zeros_like(signal) # just the crackles
+    num_crackles = int(len(signal) * density)
+    indices = np.random.randint(0, len(signal), num_crackles)
+
+    for i in indices:
+        burst_length = np.random.randint(int(0.0005 * sample_rate), int(0.002 * sample_rate))
+        end = min(i + burst_length, len(signal))
+        if end <= i:
+            continue
+
+        burst = np.random.randn(burst_length) * intensity
+        crackle_track[i:end] += burst[:end - i]
+
+    result = signal + crackle_track
+    return np.clip(result, -1, 1)
+
--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from config import *
 from core.io_utils import load_audio_to_numpy, save_wav
 from core.utils import normalize
 from core.modulation import am_modulate, am_synchronous_demodulate
-from core.noise import add_noise_bandlimited
+from core.effects import add_noise_bandlimited, add_noise_fullspectrum, add_crackle
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="Path to input audio file (.wav or .mp3)")
     args = parser.parse_args()
 
+    # Load samples
     samples, sample_rate = load_audio_to_numpy(args.input_file)
+    # Normalize
     normalized_samples = normalize(samples)
+    # Modulate
     am_signal = am_modulate(normalized_samples, sample_rate, CARRIER_FREQUENCY)
+    # Add effects
     received = add_noise_bandlimited(am_signal, sample_rate)
-    demodulated = am_synchronous_demodulate(received, sample_rate, CARRIER_FREQUENCY, LOWPASS_CUTOFF)
-    save_wav(demodulated, sample_rate, "output.wav")
+    received = add_crackle(received, sample_rate)
 
-    # Save output
+    # De-modulate
+    demodulated = am_synchronous_demodulate(received, sample_rate, CARRIER_FREQUENCY, LOWPASS_CUTOFF)
+    # Save as .WAV
     save_wav(demodulated, sample_rate, "output.wav")
     print("Saved AM demodulated audio as output.wav")
 
